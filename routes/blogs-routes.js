adcca19119da27e4e854f42d643a3fc24111414a
AkashAbrrar <mabrrar9@gmail.com>
2022-09-26 17:35:01 +0600

    delete and get by id oparaiotn is  done

--- routes/blogs-routes.js
+++ routes/blogs-routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addBlogs, getAllBlogs, updateBlog } from '../controller/blog-controller';
+import { addBlogs, deleteBlog, getAllBlogs, getById, updateBlog } from '../controller/blog-controller';
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlogs);
 blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id", getById);
+blogRouter.delete("/:id", deleteBlog);
 
 export default blogRouter;
